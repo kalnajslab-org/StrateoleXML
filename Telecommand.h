21b5680b59c948a5c1854b46d8550252d9d816b6
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-03-05 12:48:17 -0700

    Add TCs for MCB power on/off and LoRa test mode on/off

--- Telecommand.h
+++ Telecommand.h
@@ -65,6 +65,10 @@ enum Telecommand_t : uint8_t {
     RATSGETEEPROM = 61,         // Fetch the mainboard EEPROM
     RATSREALTIMEMCBON = 62,     // Enable real-time MCB reporting
     RATSREALTIMEMCBOFF = 63,    // Disable real-time MCB reporting
+    RATSMCBPOWON = 64,          // MCB power on
+    RATSMCBPOWOFF = 65,         // MCB power off
+    RATSLORATXTESTON = 66,      // Enable LoRa TX test mode
+    RATSLORATXTESTOFF = 67,     // Disable LoRa TX test mode
 
     // LPC Settings
     SETMODE = 100, // Expects mode enum
